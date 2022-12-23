13c9b1c5a0de452889e54707bcf28ea073134d83
CavacoAntoine <antoinecavaco@gmail.com>
2022-12-23 13:20:35 +0100

    3D debut

--- TP6/main.cpp
+++ TP6/main.cpp
@@ -1,9 +1,23 @@
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
+#include <glimac/Program.hpp>
+#include <glimac/FilePath.hpp>
+#include <glimac/Sphere.hpp>
+#include <glimac/common.hpp>
+#include <glimac/glm.hpp>
+#include <glimac/getTime.hpp>
+#include <glm/gtc/random.hpp>
+#include <vector>
+#include <iomanip>
 
 int window_width  = 1280;
 int window_height = 720;
+
+const GLuint VERTEX_ATTR_POSITION = 0;
+const GLuint VERTEX_ATTR_COLOR = 1;
+const GLuint VERTEX_ATTR_TEXCOORDS = 2;
+const GLuint VERTEX_ATTR_NORMAL = 3;
 
 static void key_callback(GLFWwindow* /*window*/, int /*key*/, int /*scancode*/, int /*action*/, int /*mods*/)
 {
@@ -27,8 +41,14 @@ static void size_callback(GLFWwindow* /*window*/, int width, int height)
     window_height = height;
 }
 
-int main()
+int main(int argc, char * argv[])
 {
+
+    if(argc < 3 ) {
+        std::cout << "chemin d'accès au fichier VS et FS requis\n";
+        return -1;
+    }
+
     /* Initialize the library */
     if (!glfwInit()) {
         return -1;
@@ -63,10 +83,80 @@ int main()
     glfwSetCursorPosCallback(window, &cursor_position_callback);
     glfwSetWindowSizeCallback(window, &size_callback);
 
+    glimac::FilePath applicationPath(argv[0]);
+    glimac::Program program = loadProgram(applicationPath.dirPath() + "TP6/shaders/" + argv[1],
+                              applicationPath.dirPath() + "TP6/shaders/" + argv[2]);
+    program.use();
+
+    glimac::Sphere sphere = glimac::Sphere(1,32,32);
+
+    // Création du vbo
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sphere.getVertexCount() * sizeof(glimac::ShapeVertex),(glimac::ShapeVertex *) sphere.getDataPointer(), GL_STATIC_DRAW);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // Création du vao
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(VERTEX_ATTR_POSITION);
+    glEnableVertexAttribArray(VERTEX_ATTR_TEXCOORDS);
+    glEnableVertexAttribArray(VERTEX_ATTR_NORMAL);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(VERTEX_ATTR_POSITION, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex) , (const GLvoid*)offsetof(glimac::ShapeVertex, position));
+    glVertexAttribPointer(VERTEX_ATTR_TEXCOORDS, 2, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex) , (const GLvoid*)offsetof(glimac::ShapeVertex, texCoords));
+    glVertexAttribPointer(VERTEX_ATTR_NORMAL, 3, GL_FLOAT, GL_FALSE, sizeof(glimac::ShapeVertex) , (const GLvoid*)offsetof(glimac::ShapeVertex, normal));
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+
+    // Récupéaration des location des variables uniformes
+    GLint locMVPMatrix = glGetUniformLocation(program.getGLId(), "uMVPMatrix");
+    GLint locMVMatrix = glGetUniformLocation(program.getGLId(), "uMVMatrix");
+    GLint locNormalMatrix = glGetUniformLocation(program.getGLId(), "uNormalMatrix");
+
+    glEnable(GL_DEPTH_TEST); // Permet l'activation de profondeur du GPU
+
+    glm::mat4 ProjMatrix = glm::perspective(glm::radians(70.f), (float)window_width/(float)window_height, 0.1f, 100.f);
+    glm::mat4 MVMatrix;
+    glm::mat4 NormalMatrix;
+
+    std::vector<glm::vec3> random;
+    for (int i = 0; i<32; i++) {
+        random.push_back(glm::sphericalRand(10));
+    }
+    
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window)) {
         glClearColor(0.75f, 0.75f, 0.f, 1.f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        
+        glBindVertexArray(vao);
+
+        // Terre
+        MVMatrix = glm::translate(glm::mat4(1), glm::vec3(0,0,-5));
+        NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
+        glUniformMatrix4fv(locMVPMatrix, 1, GL_FALSE, glm::value_ptr(ProjMatrix * MVMatrix));
+        glUniformMatrix4fv(locMVMatrix, 1, GL_FALSE, glm::value_ptr(MVMatrix));
+        glUniformMatrix4fv(locNormalMatrix, 1, GL_FALSE, glm::value_ptr(NormalMatrix));
+        glDrawArrays(GL_TRIANGLES, 0, sphere.getVertexCount());
+
+        for(int i = 0; i<32; i++) {
+            // Lune
+            MVMatrix = glm::translate(glm::mat4(1), glm::vec3(0, 0, -5)); // Translation
+            MVMatrix = glm::rotate(MVMatrix, glimac::getTime(), glm::normalize(random[i])); // Translation * Rotation
+            MVMatrix = glm::translate(MVMatrix, glm::vec3(-2, 0, 0)); // Translation * Rotation * Translation
+            MVMatrix = glm::scale(MVMatrix, glm::vec3(0.2, 0.2, 0.2)); // Translation * Rotation * Translation * Scale
+            NormalMatrix = glm::transpose(glm::inverse(MVMatrix));
+            glUniformMatrix4fv(locMVPMatrix, 1, GL_FALSE, glm::value_ptr(ProjMatrix * MVMatrix));
+            glUniformMatrix4fv(locMVMatrix, 1, GL_FALSE, glm::value_ptr(MVMatrix));
+            glUniformMatrix4fv(locNormalMatrix, 1, GL_FALSE, glm::value_ptr(NormalMatrix));
+            glDrawArrays(GL_TRIANGLES, 0, sphere.getVertexCount());
+        }
+        
+        glBindVertexArray(0);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
