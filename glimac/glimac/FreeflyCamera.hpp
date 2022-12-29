62d0c9d85ddee1a487018b0ab12ff12d929c800b
CavacoAntoine <antoinecavaco@gmail.com>
2022-12-29 13:42:11 +0100

    start freeflyCamera.hpp

---
+++ glimac/glimac/FreeflyCamera.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "common.hpp"
+
+namespace glimac {
+
+    class FreeflyCamera {
+        
+        public :
+            FreeflyCamera() : m_Position(glm::vec3(0,0,0)), m_fPhi(M_PI), m_fTheta(0.f)
+            {
+                computeDirectionVectors();
+            };
+
+            void computeDirectionVectors();
+            void moveLeft(float t);
+            void moveFront(float t);
+            void rotateLeft(float degrees);
+            void rortateUp(float degrees);
+
+            glm::mat4 getViewMatrix() const;
+
+        private :
+            glm::vec3 m_Position, m_FrontVector, m_LeftVector, m_UpVector;
+            float m_fPhi, m_fTheta;
+    };
+
+}
